8bba84fa2db604fb14b0fbb0b96f414aab048894
saimohanayinam <saimohanayinam@gmail.com>
2023-11-28 17:27:52 -0600

    initial branch commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import newImage from "../src/assets/react-core-concepts.png"
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const discription = reactDescriptions[genRandomInt(2)]
+  return (
+    <header>
+      <img src={newImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+       {discription} Fundamental React concepts you will need for   B`almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!hello
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
