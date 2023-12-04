cf0b43388cccfbcf5cd4ce0c1067172ba5fc85c2
saimohanayinam <saimohanayinam@gmail.com>
2023-12-03 23:32:00 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import newImage from "../src/assets/react-core-concepts.png"
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
+import newImage from "../src/assets/react-core-concepts.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const discription = reactDescriptions[genRandomInt(2)]
+  const discription = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src={newImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-       {discription} Fundamental React concepts you will need for   B`almost any app you are going to build!
+        {discription} Fundamental React concepts you will need for B`almost any
+        app you are going to build!
       </p>
     </header>
   );
 }
+
+const CoreConcept = ({imag, title, desc}) => {
+  return (
+    <li>
+      <img src={imag} alt="" />
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </li>
+  );
+};
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section>
+          <h2>CoreConcept</h2>
+          <ul>
+            <CoreConcept
+              imag={newImage}
+              title="sai"
+              desc="i am a great developer"
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
